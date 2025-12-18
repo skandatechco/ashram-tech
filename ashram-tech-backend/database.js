4c9b3e98d371efdab91cb27618fbd7255583b6a8
baholadigital <digitalmarketing@baholalabs.in>
2025-12-18 12:55:31 +0530

    Updated project files

--- ashram-tech-backend/database.js
+++ ashram-tech-backend/database.js
@@ -1,16 +1,18 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+// Create a MySQL connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'Bahola#1234',
+    database: process.env.DB_NAME || 'pooja_booking',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
+// Test the MySQL connection
 (async () => {
     try {
         const conn = await pool.getConnection();
